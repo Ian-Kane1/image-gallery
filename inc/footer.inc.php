7acab73246065acfff289835623fe227de21af42
Ian Kane <i.kane2@students.clark.edu>
2023-06-12 20:08:02 -0700

    Create footer.inc.php

---
+++ inc/footer.inc.php
@@ -0,0 +1,10 @@
+<!-- footer text -->
+<div class="row mt-5 text-center footer">
+    <div class="col">
+        <p>OneGallery - &copy;2023</p>
+    </div>
+</div>
+<script src="js/script.js"></script>
+</body>
+
+</html>
